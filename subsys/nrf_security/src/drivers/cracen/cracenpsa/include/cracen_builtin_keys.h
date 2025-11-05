a12651b3eabe3cb64334dd0e98b8b733768d4370
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2025-11-05 12:51:50 +0100

    this somehow works
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>

--- subsys/nrf_security/src/drivers/cracen/cracenpsa/include/cracen_builtin_keys.h
+++ subsys/nrf_security/src/drivers/cracen/cracenpsa/include/cracen_builtin_keys.h
@@ -4,35 +4,35 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
-#pragma once
-
 #ifndef CRACEN_PSA_BUILTIN_IDS_H
 #define CRACEN_PSA_BUILTIN_IDS_H
 
 #include "common.h"
-#include <cracen/ec_helpers.h>
-#include <cracen/mem_helpers.h>
-#include <cracen/statuscodes.h>
-#include <cracen_psa.h>
-#include <cracen_psa_eddsa.h>
-#include <cracen_psa_ecdsa.h>
-#include <cracen_psa_montgomery.h>
-#include <cracen_psa_ikg.h>
-#include <cracen_psa_rsa_keygen.h>
-#include <nrf_security_mutexes.h>
-#include <silexpk/sxops/rsa.h>
-#include <silexpk/ik.h>
-#include <stddef.h>
-#include <string.h>
+#include <psa/crypto.h>
+#include <psa/crypto_values.h>
+//#include <cracen/ec_helpers.h>
+//#include <cracen/mem_helpers.h>
+//#include <cracen/statuscodes.h>
+//#include <cracen_psa.h>
+//#include <cracen_psa_eddsa.h>
+//#include <cracen_psa_ecdsa.h>
+//#include <cracen_psa_montgomery.h>
+//#include <cracen_psa_ikg.h>
+//#include <cracen_psa_rsa_keygen.h>
+//#include <nrf_security_mutexes.h>
+//#include <silexpk/sxops/rsa.h>
+//#include <silexpk/ik.h>
+//#include <stddef.h>
+//#include <string.h>
 
-#include "tfm_plat_crypto_keys.h"
-#include "tfm_builtin_key_ids.h"
-#include "tfm_builtin_key_loader.h"
-#include "psa_manifest/pid.h"
-#include "tfm_spm_log.h"
-#include "crypto_library.h"
+//#include "tfm_plat_crypto_keys.h"
+//#include "tfm_builtin_key_ids.h"
+//#include "tfm_builtin_key_loader.h"
+//#include "psa_manifest/pid.h"
+//#include "tfm_spm_log.h"
+//#include "crypto_library.h"
 
-#include <identity_key.h>
+//#include <identity_key.h>
 
 #define NUMBER_OF_ELEMENTS_OF(x) sizeof(x)/sizeof(*x)
 
@@ -56,5 +56,9 @@ typedef struct
     psa_drv_slot_number_t key_slot_end;
     cracen_kmu_entry_type_t kmu_entry_type;
 } cracen_builtin_kmu_key_policy_t;
+
+bool cracen_builtin_ikg_usage_allowed(psa_drv_slot_number_t slot_number, const psa_key_attributes_t *attributes);
+
+bool cracen_builtin_kmu_usage_allowed(psa_drv_slot_number_t slot_number, const psa_key_attributes_t *attributes);
 
 #endif /* CRACEN_PSA_BUILTIN_IDS_H */
--- subsys/nrf_security/src/drivers/cracen/cracenpsa/src/cracen_builtin_keys.c
+++ subsys/nrf_security/src/drivers/cracen/cracenpsa/src/cracen_builtin_keys.c
@@ -6,29 +6,33 @@
 
 
 #include "common.h"
-#include <cracen/ec_helpers.h>
-#include <cracen/mem_helpers.h>
-#include <cracen/statuscodes.h>
-#include <cracen_psa.h>
-#include <cracen_psa_eddsa.h>
-#include <cracen_psa_ecdsa.h>
-#include <cracen_psa_montgomery.h>
-#include <cracen_psa_ikg.h>
-#include <cracen_psa_rsa_keygen.h>
-#include <nrf_security_mutexes.h>
-#include <silexpk/sxops/rsa.h>
-#include <silexpk/ik.h>
+
+#include <psa/crypto.h>
+#include <psa/crypto_values.h>
+
+//#include <cracen/ec_helpers.h>
+//#include <cracen/mem_helpers.h>
+//#include <cracen/statuscodes.h>
+//#include <cracen_psa.h>
+//#include <cracen_psa_eddsa.h>
+//#include <cracen_psa_ecdsa.h>
+//#include <cracen_psa_montgomery.h>
+//#include <cracen_psa_ikg.h>
+//#include <cracen_psa_rsa_keygen.h>
+//#include <nrf_security_mutexes.h>
+//#include <silexpk/sxops/rsa.h>
+//#include <silexpk/ik.h>
 #include <stddef.h>
 #include <string.h>
 
-#include "tfm_plat_crypto_keys.h"
-#include "tfm_builtin_key_ids.h"
-#include "tfm_builtin_key_loader.h"
+//#include "tfm_plat_crypto_keys.h"
+//#include "tfm_builtin_key_loader.h"
 #include "psa_manifest/pid.h"
-#include "tfm_spm_log.h"
+#include "tfm_builtin_key_ids.h"
+//#include "tfm_spm_log.h"
 #include "crypto_library.h"
 
-#include <identity_key.h>
+//#include <identity_key.h>
 #include <cracen_builtin_keys.h>
 
 const cracen_builtin_ikg_key_policy_t g_builtin_ikg_policy[] = { 
@@ -41,11 +45,11 @@ const cracen_builtin_ikg_key_policy_t g_builtin_ikg_policy[] = {
 const cracen_builtin_kmu_key_policy_t g_builtin_kmu_policy[] = { 
     { .user = TFM_SP_ITS, .key_slot_start = 0, .key_slot_end = 255, .kmu_entry_type = KMU_ENTRY_SLOT_RANGE},
     { .user = TFM_SP_CRYPTO, .key_slot_start = 0, .key_slot_end = 255, .kmu_entry_type = KMU_ENTRY_SLOT_RANGE},
-    { .user = MAPPED_TZ_NS_AGENT_DEFAULT_CLIENT_ID, .key_slot_start = 0, .key_slot_end = 90, .kmu_entry_type = KMU_ENTRY_SLOT_RANGE}
+    { .user = 0xc4000000, .key_slot_start = 0, .key_slot_end = 40, .kmu_entry_type = KMU_ENTRY_SLOT_RANGE}
 };
 
 
-bool cracen_builtin_ikg_usage_allowed(psa_drv_slot_number_t slot_number, psa_key_attributes_t *attributes)
+bool cracen_builtin_ikg_usage_allowed(psa_drv_slot_number_t slot_number, const psa_key_attributes_t *attributes)
 {
     tfm_crypto_library_key_id_t key_id = psa_get_key_id(attributes);
 
@@ -58,15 +62,15 @@ bool cracen_builtin_ikg_usage_allowed(psa_drv_slot_number_t slot_number, psa_key
     return false;
 }
 
-bool cracen_builtin_kmu_usage_allowed(psa_drv_slot_number_t slot_number, psa_key_attributes_t *attributes)
+bool cracen_builtin_kmu_usage_allowed(psa_drv_slot_number_t slot_number, const psa_key_attributes_t *attributes)
 {
     tfm_crypto_library_key_id_t key_id = psa_get_key_id(attributes);
 
     volatile int32_t owner = CRYPTO_LIBRARY_GET_OWNER(key_id);
 
-    while(owner == 0xF3F3F5F5)
+    if(owner == 0x0)
     {
-        return false;
+        return true;
     }
 
     for(uint32_t i = 0; i < NUMBER_OF_ELEMENTS_OF(g_builtin_kmu_policy); i++) {
@@ -81,7 +85,7 @@ bool cracen_builtin_kmu_usage_allowed(psa_drv_slot_number_t slot_number, psa_key
             break;
         
         case KMU_ENTRY_SLOT_RANGE:
-            if(g_builtin_kmu_policy[i].user == CRYPTO_LIBRARY_GET_OWNER(key_id)  && (g_builtin_kmu_policy[i].key_slot_start >= slot_number && g_builtin_kmu_policy[i].key_slot_end <= slot_number))
+            if(g_builtin_kmu_policy[i].user == CRYPTO_LIBRARY_GET_OWNER(key_id)  && (g_builtin_kmu_policy[i].key_slot_start <= slot_number && g_builtin_kmu_policy[i].key_slot_end >= slot_number))
             {
                 return true;
             } 
