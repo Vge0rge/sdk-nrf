bcbb4ef963b122489df6c9fff726d4c1d20c4d0e
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2026-05-12 16:49:18 +0200

    modules: tf-m: Update logging system
    TF-M 2.3 introduced a new logging system where
    it replaced tfm_sp_log.h / tfm_spm_log.h / SPMLOG_* with two new headers:
    
    tfm_log.h — For privileged code which provides macros: ERROR/INFO/VERBOSE/etc
    tfm_log_unpriv.h — Unprivileged code with the same macros suffixed _UNPRIV
    
    Adapt to the new logging system.
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>

--- modules/trusted-firmware-m/tfm_boards/include/zephyr/logging/log.h
+++ modules/trusted-firmware-m/tfm_boards/include/zephyr/logging/log.h
@@ -15,22 +15,35 @@
  * The functionality will be forwarded to TF-M equivalent of the Zephyr API.
  */
 
-#include "tfm_sp_log.h"
-#include <tfm_spm_log.h>
+/*
+ * TF-M's old per-partition logger (tfm_sp_log.h) and SPM logger (tfm_spm_log.h)
+ * were replaced by the new unprivileged logging API (tfm_log_unpriv.h) in
+ * upstream commit 1f2c7fdb4 ("LIB: Move partitions to use new logging API").
+ *
+ * The macros LOG_ERRFMT/LOG_WRNFMT/LOG_INFFMT/LOG_DBGFMT and SPMLOG_*MSG no
+ * longer exist; they are replaced by ERROR_UNPRIV/WARN_UNPRIV/INFO_UNPRIV/
+ * VERBOSE_UNPRIV (and the *_RAW variants).
+ *
+ * Consumers of this header must link against a TF-M target that pulls in
+ * tfm_log_unpriv_headers and defines LOG_LEVEL_UNPRIV (e.g. tfm_sprt or
+ * tfm_config).
+ */
+#include "tfm_log_unpriv.h"
 
 #define LOG_MODULE_DECLARE(...)
 #define LOG_MODULE_REGISTER(...)
 
-#define LOG_ERR(fmt, ...) LOG_ERRFMT(fmt "\r\n", ##__VA_ARGS__)
-#define LOG_WRN(fmt, ...) LOG_WRNFMT(fmt "\r\n", ##__VA_ARGS__)
-#define LOG_INF(fmt, ...) LOG_INFFMT(fmt "\r\n", ##__VA_ARGS__)
-#define LOG_DBG(fmt, ...) LOG_DBGFMT(fmt "\r\n", ##__VA_ARGS__)
+#define LOG_ERR(fmt, ...) ERROR_UNPRIV(fmt "\r\n", ##__VA_ARGS__)
+#define LOG_WRN(fmt, ...) WARN_UNPRIV(fmt "\r\n", ##__VA_ARGS__)
+#define LOG_INF(fmt, ...) INFO_UNPRIV(fmt "\r\n", ##__VA_ARGS__)
+#define LOG_DBG(fmt, ...) VERBOSE_UNPRIV(fmt "\r\n", ##__VA_ARGS__)
 
 /* This can be used for simple messages before the SPM is initialized in thread
- * mode
+ * mode. The legacy SPMLOG_*MSG macros are no longer available; route these to
+ * the unprivileged logging API as well, which is the closest equivalent.
  */
-#define LOG_ERR_MSG(msg) SPMLOG_ERRMSG(msg "\r\n")
-#define LOG_INF_MSG(msg) SPMLOG_INFMSG(msg "\r\n")
-#define LOG_DBG_MSG(msg) SPMLOG_DBGMSG(msg "\r\n")
+#define LOG_ERR_MSG(msg) ERROR_UNPRIV(msg "\r\n")
+#define LOG_INF_MSG(msg) INFO_UNPRIV(msg "\r\n")
+#define LOG_DBG_MSG(msg) VERBOSE_UNPRIV(msg "\r\n")
 
 #endif /* __ZEPHYR_LOGGING_LOG_H__ */
